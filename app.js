ee66c31bcaec3f7928a330af0bba00c80561cb20
Ravindra <rk2671353@gmail.com>
2023-06-28 11:59:21 +0000

    COdE>>

--- app.js
+++ app.js
@@ -68,14 +68,14 @@ app.post("/login", async (request, response) => {
 
   if (dbUser === undefined) {
     response.status(400);
-    response.send("Invalid User");
+    response.send("Invalid user");
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
     if (isPasswordMatched === true) {
-      response.send("Login Success!");
+      response.send("Login success!");
     } else {
       response.status(400);
-      response.send("Invalid Password");
+      response.send("Invalid password");
     }
   }
 });
